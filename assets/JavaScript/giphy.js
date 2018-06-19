2bc62d824d8cda732b7c6c3ef65dfdc4117cd592
unknown <lakis004@umn.edu>
2018-06-18 23:11:24 -0500

    updated

--- assets/JavaScript/giphy.js
+++ assets/JavaScript/giphy.js
@@ -3,7 +3,10 @@
   var gifs = ["Mouse"];
 
   //displayGifInfo function renders the HTML to display content
-    $("button").on("click", function displaygifsInfo() {
+  $("button").on("click", function () {
+    displaygifsInfo();
+  });
+    function displaygifsInfo() {
       
       var person = $(this).attr("data-person");
       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -39,7 +42,7 @@
             
           }
         });
-    });
+    }
     
 
     
@@ -47,13 +50,13 @@
 
     // Function for displaying movie data
 function renderButtons() {
-
+console.log("RenderFunction")
   // Deletes the movies prior to adding new movies
   // (this is necessary otherwise you will have repeat buttons)
   $("#buttons-view").empty();
 
   // Loops through the array of movies
-  for (var i = 0; i < movies.length; i++) {
+  for (var i = 0; i < gifs.length; i++) {
 
       // Then dynamicaly generates buttons for each movie in the array
       // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
@@ -61,9 +64,9 @@ function renderButtons() {
       // Adds a class of movie to our button
       a.addClass("movie");
       // Added a data-attribute
-      a.attr("data-name", movies[i]);
+      a.attr("data-name", gifs[i]);
       // Provided the initial button text
-      a.text(movies[i]);
+      a.text(gifs[i]);
       // Added the button to the buttons-view div
       $("#buttons-view").append(a);
   }
@@ -76,7 +79,7 @@ $("#add-movie").on("click", function (event) {
   var movie = $("#movie-input").val().trim();
 
   // The movie from the textbox is then added to our array
-  movies.push(movie);
+  gifs.push(movie);
 
   // Calling renderButtons which handles the processing of our movie array
   renderButtons();
@@ -84,7 +87,7 @@ $("#add-movie").on("click", function (event) {
 });
 
 // Adding click event listeners to all elements with a class of "movie"
-$(document).on("click", ".movie", displayMovieInfo);
+$(document).on("click", ".movie", displaygifsInfo);
 
 // Calling the renderButtons function to display the intial buttons
 renderButtons();
@@ -94,7 +97,6 @@ $(document).on("click", ".gif", function () {
   // If the clicked image's state is still, update its src attribute to what its data-animate value is.
   // Then, set the image's data-state to animate
   // Else set src to the data-still value
-  console.log(this)
   if (state === "still") {
       $(this).attr("src", $(this).attr("data-animate"));
       $(this).attr("data-state", "animate");
